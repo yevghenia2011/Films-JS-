4c269993ffb232f1764be71bc765f99a60fa7485
Jane <yevghenia2011@gmail.com>
2022-12-13 13:23:03 +0200

    first_lesson_19

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,24 @@
 "use strict";
 
-const numberOffFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посморели?", "");
+console.log(numberOfFilms);
 
 const personalMovieDB = {
-        count: numberOffFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const last1 = prompt("Один из последних просмотренных фильмов?", "");
-const last2 = prompt("Один из последних просмотренных фильмов?", "");
-const rate1 = prompt("На сколько оцените его?", "");
-const rate2 = prompt("На сколько оцените его?", "");
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = +prompt("На сколько оцените его?", ""),
+        c = prompt("Один из последних просмотренных фильмов?", ""),
+        d = +prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[last1] = rate1;
-personalMovieDB.movies[last2] = rate2;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
